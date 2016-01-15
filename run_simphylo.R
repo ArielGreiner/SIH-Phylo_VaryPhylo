df4cc47bde46c2f9f628623835074d15ad8e312c
ArielGreiner <ariel.kiara@gmail.com>
2016-01-15 16:41:24 -0500

    changed # of replicates from 2 to 100

--- run_simphylo.R
+++ run_simphylo.R
@@ -1,6 +1,6 @@
 nspecies<-7 #the number of species
 npatches<-10 #the number of patches
-nreplicates<-2 #number of replicates
+nreplicates<-100 #number of replicates
 nfunctions<-1
 
 DispV<-c(0.0001,0.0005,0.001,0.005,0.01,0.05,0.1,0.5,1) #the dispersal rates 
